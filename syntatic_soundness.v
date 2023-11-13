14d4f323c0a759b221563a7d492a91b2787eba2f
Yiyun Liu <liuyiyun@seas.upenn.edu>
2023-11-13 16:49:31 -0500

    Finish fixing syntactic soundness

--- syntatic_soundness.v
+++ syntatic_soundness.v
@@ -120,30 +120,30 @@ Lemma weakening_Syn' Γ a A A0 B i
   Wt (B :: Γ) (ren_tm shift a) A0.
 Proof. sfirstorder use:weakening_Syn. Qed.
 
-Definition good_morphing ρ n Γ m Δ :=
-  forall i, i < n -> Wt m Δ (ρ i) (subst_tm ρ (dep_ith Γ i)).
+Definition good_morphing ρ Γ Δ :=
+  forall i, i < length Γ -> Wt Δ (ρ i) (subst_tm ρ (dep_ith Γ i)).
 
-Lemma T_Var' n Γ i A :
+Lemma T_Var' Γ i A :
   A = dep_ith Γ i ->
-  Wff n Γ ->
-  i < n ->
+  Wff Γ ->
+  i < length Γ ->
   (* ------ *)
-  Wt n Γ (var_tm i) A.
+  Wt Γ (var_tm i) A.
 Proof. qauto ctrs:Wt. Qed.
 
-Lemma good_morphing_up ρ n k Γ m Δ A
-  (h : good_morphing ρ n Γ m Δ) :
-  Wff m Δ ->
-  Wt m Δ (subst_tm ρ A) (tUniv k) ->
-  good_morphing (up_tm_tm ρ) (S n) (A .: Γ) (S m) (subst_tm ρ A .: Δ).
+Lemma good_morphing_up ρ k Γ Δ A
+  (h : good_morphing ρ Γ Δ) :
+  Wff Δ ->
+  Wt Δ (subst_tm ρ A) (tUniv k) ->
+  good_morphing (up_tm_tm ρ) (A :: Γ) (subst_tm ρ A :: Δ).
 Proof.
   rewrite /good_morphing => h1 h2.
   case => [_ | i /Arith_prebase.lt_S_n ?].
-  - apply T_Var'.
-    + by asimpldep.
+  - apply T_Var' => /=.
+    + by asimpl.
     + eauto with wff.
     + asimpl. lia.
-  - rewrite dep_ith_ren_tm.
+  - simpl.
     apply : weakening_Syn'; cycle 2.
     rewrite /good_morphing in h.
     + sfirstorder unfold:good_morphing.
@@ -151,12 +151,12 @@ Proof.
     + sfirstorder.
 Qed.
 
-Lemma morphing_Syn n Γ a A (h : Wt n Γ a A) : forall m Δ ρ,
-    good_morphing ρ n Γ m Δ ->
-    Wff m Δ ->
-    Wt m Δ (subst_tm ρ a) (subst_tm ρ A).
+Lemma morphing_Syn Γ a A (h : Wt Γ a A) : forall Δ ρ,
+    good_morphing ρ Γ Δ ->
+    Wff Δ ->
+    Wt Δ (subst_tm ρ a) (subst_tm ρ A).
 Proof.
-  elim : n Γ a A / h; try qauto l:on depth:1 ctrs:Wt unfold:good_morphing.
+  elim : Γ a A / h; try qauto l:on depth:1 ctrs:Wt unfold:good_morphing.
   - move => *.
     apply T_Pi; eauto.
     hauto q:on use:good_morphing_up db:wff.
@@ -167,39 +167,37 @@ Proof.
   - hauto q:on ctrs:Wt use:join_subst_star.
 Qed.
 
-Lemma subst_Syn n Γ A a b B
-  (h0 : Wt (S n) (A .: Γ) b B)
-  (h1 : Wt n Γ a A) :
-  Wt n Γ (subst_tm (a..) b) (subst_tm (a..) B).
+Lemma subst_Syn Γ A a b B
+  (h0 : Wt (A :: Γ) b B)
+  (h1 : Wt Γ a A) :
+  Wt Γ (subst_tm (a..) b) (subst_tm (a..) B).
 Proof.
   apply : morphing_Syn; eauto with wff.
-  case => [_ | i /Arith_prebase.lt_S_n ?].
-  - rewrite dep_ith_ren_tm0.
-    by asimpl.
-  - rewrite dep_ith_ren_tm.
-    asimpl.
-    eauto using T_Var with wff.
+  case => [_ | i /Arith_prebase.lt_S_n ?] /=.
+  - by asimpl.
+  - asimpl; eauto using T_Var with wff.
 Qed.
 
-Lemma Wt_regularity n Γ a A
-  (h : Wt n Γ a A) :
-  exists i, Wt n Γ A (tUniv i).
+Lemma Wt_regularity Γ a A
+  (h : Wt Γ a A) :
+  exists i, Wt Γ A (tUniv i).
 Proof.
-  elim:n Γ a A/h; try qauto ctrs:Wt depth:2.
-  - inversion 1; qauto l:on use:good_renaming_truncate', renaming_Syn.
+  elim: Γ a A/h; try qauto ctrs:Wt depth:2.
+  - inversion 1.
+    hauto l:on use:dep_ith_shift,good_renaming_truncate, renaming_Syn.
   - hauto q:on use:subst_Syn, Wt_Pi_Univ_inv.
 Qed.
 
-Lemma Wt_App_inv n Γ b a T (h : Wt n Γ (tApp b a) T) :
-  exists A B, Wt n Γ b (tPi A B) /\
-         Wt n Γ a A /\
+Lemma Wt_App_inv Γ b a T (h : Wt Γ (tApp b a) T) :
+  exists A B, Wt Γ b (tPi A B) /\
+         Wt Γ a A /\
          Join (subst_tm (a..) B) T /\
-         exists i, Wt n Γ T (tUniv i).
+         exists i, Wt Γ T (tUniv i).
 Proof.
   move E : (tApp b a) h => ba h.
   move : b a E.
-  elim : n Γ ba T /h => //.
-  - move => n Γ a A B b h0 _ h1 _ ? ? [] *; subst.
+  elim : Γ ba T /h => //.
+  - move => Γ a A B b h0 _ h1 _ ? ? [] *; subst.
     exists A, B; repeat split => //.
     + apply join_subst_star. apply Join_reflexive.
     + move /Wt_regularity : h0.
@@ -211,31 +209,31 @@ Proof.
 Qed.
 
 
-Lemma Wt_If_inv n Γ a b c T (h : Wt n Γ (tIf a b c) T) :
-  exists A, Wt n Γ a tSwitch /\
-         Wt n Γ b A /\
-         Wt n Γ c A /\
+Lemma Wt_If_inv Γ a b c T (h : Wt Γ (tIf a b c) T) :
+  exists A, Wt Γ a tSwitch /\
+         Wt Γ b A /\
+         Wt Γ c A /\
          Join A T /\
-         exists i, Wt n Γ T (tUniv i).
+         exists i, Wt Γ T (tUniv i).
 Proof.
   move E : (tIf a b c) h => a0 h.
   move : a b c E.
-  elim : n Γ a0 T / h => //.
+  elim : Γ a0 T / h => //.
   - hauto lq:on rew:off use:Join_transitive.
   - qauto l:on use:Join_reflexive, Wt_regularity.
 Qed.
 
-Lemma preservation_helper n A0 A1 i j Γ a A :
-  Wt (S n) (A0, Γ) a A ->
-  Wt n Γ A0 (tUniv i) ->
-  Wt n Γ A1 (tUniv j) ->
+Lemma preservation_helper A0 A1 i j Γ a A :
+  Wt (A0 :: Γ) a A ->
+  Wt Γ A0 (tUniv i) ->
+  Wt Γ A1 (tUniv j) ->
   Join A0 A1 ->
-  Wt (S n) (A1, Γ) a A.
+  Wt (A1 :: Γ) a A.
 Proof.
   move => h0 h1 h2 h3.
   replace a with (subst_tm ids a); last by asimpl.
   replace A with (subst_tm ids A); last by asimpl.
-  apply morphing_Syn with (n := S n) (Γ := A0 .: Γ).
+  apply morphing_Syn with (Γ := A0 :: Γ).
   - done.
   - case => [_ | k /Arith_prebase.lt_S_n ?].
     + rewrite dep_ith_ren_tm0; asimpl.
@@ -244,24 +242,23 @@ Proof.
       * change (tUniv i) with (ren_tm shift (tUniv i)).
         apply weakening_Syn with (i := j) => //.
       * hauto lq:on use:Join_symmetric, join_renaming.
-    + rewrite dep_ith_ren_tm.
-      asimpl.
+    + asimpl.
       change (var_tm (S k)) with (ren_tm shift (var_tm k)).
       apply weakening_Syn with (i := j) => //.
       apply T_Var; hauto lq:on db:wff.
   - eauto with wff.
 Qed.
 
-Lemma subject_reduction a b (h : Par a b) : forall n Γ A,
-    Wt n Γ a A -> Wt n Γ b A.
+Lemma subject_reduction a b (h : Par a b) : forall Γ A,
+    Wt Γ a A -> Wt Γ b A.
 Proof.
   elim : a b /h => //.
-  - move => A0 A1 B0 B1 h0 ih0 h1 ih1 n Γ A /Wt_Pi_inv.
+  - move => A0 A1 B0 B1 h0 ih0 h1 ih1 Γ A /Wt_Pi_inv.
     intros (i & hA0 & hAB0 & hAJoin & j & hA).
-    have ? : Wff n Γ by eauto with wff.
+    have ? : Wff Γ by eauto with wff.
     apply T_Conv with (A := tUniv i) (i := j) => //.
     qauto l:on ctrs:Wt use:preservation_helper, Par_join.
-  - move => A0 A1 a0 a1 h0 ih0 h1 ih1 n Γ A /Wt_Abs_inv.
+  - move => A0 A1 a0 a1 h0 ih0 h1 ih1 Γ A /Wt_Abs_inv.
     intros (B & i & hPi & ha0 & hJoin & j & hA).
     case /Wt_Pi_Univ_inv : hPi => hA0 hB.
     apply T_Conv with (A := tPi A1 B) (i := j) => //.
@@ -272,7 +269,7 @@ Proof.
       apply Join_symmetric.
       apply Par_join.
       hauto lq:on ctrs:Par use:Par_refl.
-  - move => a0 a1 b0 b1 h0 ih0 h1 ih1 n Γ A /Wt_App_inv.
+  - move => a0 a1 b0 b1 h0 ih0 h1 ih1 Γ A /Wt_App_inv.
     intros (A0 & B & hPi & hb0 & hJoin & i & hA).
     eapply T_Conv with (A := subst_tm (b1..) B); eauto.
     apply : T_App; eauto.
@@ -281,7 +278,7 @@ Proof.
     apply Par_join.
     apply par_morphing; last by apply Par_refl.
     hauto q:on ctrs:Par inv:nat simp:asimpl.
-  - move => a A a0 b0 b1 haa0 iha hbb0 ihb n Γ A0 /Wt_App_inv.
+  - move => a A a0 b0 b1 haa0 iha hbb0 ihb Γ A0 /Wt_App_inv.
     intros (A1 & B & ha & hb0 & hJoin & i & hA0).
     have /iha /Wt_Abs_inv := ha; intros (B0 & k & hPi & ha0 & hJoin' & j & hPi').
     case /join_pi_inj : hJoin' => *.
@@ -320,36 +317,36 @@ Inductive head := hPi | hAbs | hSwitch | hOn | hOff | hFalse | hUniv | hVar.
 
 Definition tm_to_head (a : tm) :=
   match a with
-  | tPi A B => O.Some hPi
-  | tAbs A a => O.Some hAbs
-  | tSwitch => O.Some hSwitch
-  | tOn => O.Some hOn
-  | tOff => O.Some hOff
-  | tFalse => O.Some hFalse
-  | tIf a b c => O.None
-  | tApp a b => O.None
-  | tUniv _ => O.Some hUniv
-  | var_tm _ => O.Some hVar
+  | tPi A B => Some hPi
+  | tAbs A a => Some hAbs
+  | tSwitch => Some hSwitch
+  | tOn => Some hOn
+  | tOff => Some hOff
+  | tFalse => Some hFalse
+  | tIf a b c => None
+  | tApp a b => None
+  | tUniv _ => Some hUniv
+  | var_tm _ => Some hVar
   end.
 
 Lemma par_head a b (h : Par a b) :
-  forall hd, tm_to_head a = O.Some hd ->
-        tm_to_head b = O.Some hd.
+  forall hd, tm_to_head a = Some hd ->
+        tm_to_head b = Some hd.
 Proof. induction h => //. Qed.
 
 Lemma par_head_star a b (h : Rstar _ Par a b) :
-  forall hd, tm_to_head a = O.Some hd ->
-        tm_to_head b = O.Some hd.
+  forall hd, tm_to_head a = Some hd ->
+        tm_to_head b = Some hd.
 Proof. induction h; eauto using par_head. Qed.
 
 Lemma join_consistent a b (h : Join a b) :
-  forall hd hd1, tm_to_head a = O.Some hd ->
-            tm_to_head b = O.Some hd1 ->
+  forall hd hd1, tm_to_head a = Some hd ->
+            tm_to_head b = Some hd1 ->
             hd = hd1.
 Proof. qblast use:par_head_star. Qed.
 
-Lemma Wt_Univ_winv n Γ i U :
-  Wt n Γ (tUniv i) U ->
+Lemma Wt_Univ_winv Γ i U :
+  Wt Γ (tUniv i) U ->
   exists j, Join (tUniv j) U.
 Proof.
   move E : (tUniv i) => U0 h.
@@ -357,8 +354,8 @@ Proof.
   induction h => //; qauto l:on ctrs:Wt use:Join_transitive, Join_reflexive.
 Qed.
 
-Lemma Wt_False_winv n Γ U :
-  Wt n Γ tFalse U ->
+Lemma Wt_False_winv Γ U :
+  Wt Γ tFalse U ->
   exists j, Join (tUniv j) U.
 Proof.
   move E : tFalse => U0 h.
@@ -366,20 +363,20 @@ Proof.
   induction h => //; qauto l:on ctrs:Wt use:Join_transitive, Join_reflexive.
 Qed.
 
-Lemma Wt_On_Off_winv n Γ a A (h : Wt n Γ a A) :
+Lemma Wt_On_Off_winv Γ a A (h : Wt Γ a A) :
   is_bool_val a -> Join tSwitch A.
 Proof. induction h => //; qauto l:on ctrs:Wt use:Join_transitive, Join_reflexive. Qed.
 
-Lemma Wt_Switch_winv n Γ A :
-  Wt n Γ tSwitch A ->
+Lemma Wt_Switch_winv Γ A :
+  Wt Γ tSwitch A ->
   exists i, Join (tUniv i) A.
 Proof.
   move E : tSwitch => a h. move : E.
   induction h => //; qauto l:on ctrs:Wt use:Join_transitive, Join_reflexive.
 Qed.
 
-Lemma wt_pi_canon Γ a A B :
-  Wt 0 Γ a (tPi A B) ->
+Lemma wt_pi_canon a A B :
+  Wt nil a (tPi A B) ->
   is_value a ->
   exists A a0, a = tAbs A a0.
 Proof.
@@ -393,8 +390,8 @@ Proof.
   - qauto l:on use:Wt_Switch_winv, join_consistent.
 Qed.
 
-Lemma wt_switch_canon Γ a :
-  Wt 0 Γ a tSwitch ->
+Lemma wt_switch_canon a :
+  Wt nil a tSwitch ->
   is_value a ->
   is_bool_val a.
 Proof.
@@ -406,11 +403,11 @@ Proof.
   - qauto l:on use:Wt_Switch_winv, join_consistent.
 Qed.
 
-Lemma wt_progress Γ a A (h :Wt 0 Γ a A) : is_value a \/ exists a0, Par a a0.
+Lemma wt_progress a A (h :Wt nil a A) : is_value a \/ exists a0, Par a a0.
 Proof.
-  move E : 0 h => n h.
+  move E : nil h => Γ h.
   move : E.
-  elim: n Γ a A/h; try hauto q:on depth:2.
+  elim: Γ a A/h; try hauto q:on depth:2.
   - hauto lq:on rew:off ctrs:Par use:wt_pi_canon, Par_refl.
   - hauto lq:on rew:off use:wt_switch_canon, Par_refl.
 Qed.
