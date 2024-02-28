2690ac0fee1a9d7e8b90477a99f116dbb68ae8b3
Yiyun Liu <liuyiyun@seas.upenn.edu>
2024-02-28 18:34:14 -0500

    Fix linums

---
+++ beta/count-beta.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+tokei -f semtypingopen.v soundnessopen.v typing.v join.v normalform.v
---
+++ eta/count-eta.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+tokei -f semtyping.v soundness.v typing.v join.v normalform.v
